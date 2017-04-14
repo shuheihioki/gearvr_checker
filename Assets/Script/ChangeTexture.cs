2e86724fd3e2de1d583a92c67f80297445add749
Shuhei Hioki <hioki_shuhei@takenaka-co.co.jp>
2017-04-14 21:17:09 +0900

    added move and change texture script.

---
+++ Assets/Script/ChangeTexture.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeTexture : MonoBehaviour {
+	
+	public Texture[] _textures;
+	public Renderer _rend;
+	public string _keyA;
+	public string _keyB;
+	int _index = 0;
+	
+	bool isRunning = false;
+	
+	IEnumerator changeTex(float _sec){
+		if (isRunning)
+			yield break;
+		isRunning = true;
+		
+		int _texNum = _textures.Length;
+		
+		//Debug.Log("Corutine");
+		
+		if (Input.GetKey(_keyA))
+		{
+			_index += 1;
+			
+			if ( _index >= _texNum )
+			{
+				_index = 0;
+			}
+			_rend.material.mainTexture = _textures[_index];	
+		}
+		
+		if (Input.GetKey(_keyB))
+		{
+			_index -= 1;
+			
+			if ( _index < 0 )
+			{
+				_index = _texNum - 1;
+			}
+			_rend.material.mainTexture = _textures[_index];	
+		}
+		
+		isRunning = false;
+		
+		Debug.Log("index" + _index);
+		//Debug.Log("Tex" + _texNum);
+		
+		yield return new WaitForSeconds(_sec);
+		//yield break;
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+		_rend = GetComponent<Renderer>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		StartCoroutine(changeTex(5));
+		
+	}
+}
---
+++ Assets/Script/MoveCtrl.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCtrl : MonoBehaviour {
+
+    public GameObject _moveNode;
+    public float _offset;
+    public string _keyForward;
+    public string _keyBack;
+    public string _keyUp;
+    public string _keyDown;
+
+    void moving()
+    {
+
+        if (Input.GetKey(_keyForward))
+        {
+	        _moveNode.transform.Translate(Vector3.forward * Time.deltaTime * _offset, Space.World);
+        }
+
+        if (Input.GetKey(_keyBack))
+        {
+	        _moveNode.transform.Translate(Vector3.back * Time.deltaTime * _offset, Space.World);
+        }
+
+        if (Input.GetKey(_keyUp))
+        {
+            _moveNode.transform.Translate(Vector3.up * Time.deltaTime * _offset, Space.World);
+        }
+
+        if (Input.GetKey(_keyDown))
+        {
+            _moveNode.transform.Translate(Vector3.down * Time.deltaTime * _offset, Space.World);
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        moving();
+		
+	}
+}
